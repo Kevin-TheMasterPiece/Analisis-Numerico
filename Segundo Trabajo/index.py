de0ad46b27a801d19f5c89366bae93e62b221e21
Mateo Beltran <mabelcha13@gmail.com>
2025-02-24 16:20:15 -0500

    Numeros arabicos a romanos

--- Segundo Trabajo/index.py
+++ Segundo Trabajo/index.py
@@ -1,4 +1,11 @@
-
+def numerosRomanos(numero):
+    romanos= {1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L', 90: 'XC', 100: 'C', 400: 'CD', 500: 'D', 900: 'CM', 1000: 'M'}
+    resultado = ''
+    for valor, letra in sorted(romanos.items(), reverse=True):
+        while numero >= valor:
+            resultado += letra
+            numero -= valor
+    return resultado
 
 opcion = 0
 
@@ -13,8 +20,13 @@ while opcion != 4:
 
     if opcion == 1:
         numero = int(input("Ingresa el numero: "))
+
     elif opcion == 2:
         numero = int(input("Ingresa el numero: "))
+        if numero <= 0 or numero>4000:
+            print("No se aceptan numeros negativos ni numeros mayores a 3999")
+        else:
+            print(numerosRomanos(numero))
     elif opcion == 3:
         print("Cuadratica")
     elif opcion == 4:
