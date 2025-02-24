d4ec9dad0e3532be12aa57cf9dce011bc0cb3888
Mateo Beltran <mabelcha13@gmail.com>
2025-02-24 16:26:38 -0500

    Mejoro el programa de numeros romanos

--- Segundo Trabajo/index.py
+++ Segundo Trabajo/index.py
@@ -1,12 +1,11 @@
 def numerosRomanos(numero):
     romanos= {1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L', 90: 'XC', 100: 'C', 400: 'CD', 500: 'D', 900: 'CM', 1000: 'M'}
     resultado = ''
-    for valor, letra in sorted(romanos.items(), reverse=True):
-        while numero >= valor:
-            resultado += letra
-            numero -= valor
+    for valor in sorted(romanos.keys(), reverse=True):
+     while numero >= valor:
+        resultado += romanos[valor]
+        numero -= valor
     return resultado
-
 opcion = 0
 
 while opcion != 4:
