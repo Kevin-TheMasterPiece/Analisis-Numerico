19d38b5e551b91394700bb3387d22d69412a105d
castiwhite33 <143620627+castiwhite33@users.noreply.github.com>
2025-02-25 10:23:29 -0500

    se agrega en el segundo trabajo la parte de la ecuación cuadratica

--- Segundo Trabajo/index.py
+++ Segundo Trabajo/index.py
@@ -1,4 +1,20 @@
-
+import math
+class cuadratica:
+   
+    @staticmethod
+    def solucionar(a,b,c):
+      tipo_S = b**2 - 4*a*c
+      if tipo_S > 0:
+        x1 = (-b + math.sqrt(tipo_S)) / (2*a)
+        x2 = (-b - math.sqrt(tipo_S)) / (2*a)
+        print(f"Las soluciones son x1 = {x1} y x2 = {x2}")
+      elif tipo_S == 0:
+        x = -b / (2*a)
+        print(f"La solución única es x = {x}")
+      else:
+        x_r = -b / (2*a)
+        x_i = math.sqrt(abs(tipo_S)) / (2*a)
+        print(f"Las soluciones son complejas: x1 = {x_r} + {x_i}i y x2 = {x_r} - {x_i}i")
 
 opcion = 0
 
@@ -15,8 +31,15 @@ while opcion != 4:
         numero = int(input("Ingresa el numero: "))
     elif opcion == 2:
         numero = int(input("Ingresa el numero: "))
-    elif opcion == 3:
-        print("Cuadratica")
+    elif opcion == 3: 
+        a = float(input("Ingrese a: "))  
+        b = float(input("Ingrese b: "))
+        c = float(input("Ingrese c: "))
+        if a == 0:
+          print("El coeficiente 'a' no puede ser cero en una ecuación cuadrática.")
+        else: 
+           cuadratica.solucionar(a, b, c)
+
     elif opcion == 4:
         print("Saliendo del programa")
     else:
