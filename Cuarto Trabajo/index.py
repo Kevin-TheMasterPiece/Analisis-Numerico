9758235c1a1fcc3dd260760f1cfbc3c8c8b9fb0d
castiwhite33 <143620627+castiwhite33@users.noreply.github.com>
2025-03-05 11:05:48 -0500

    agrego cambio a octal

--- Cuarto Trabajo/index.py
+++ Cuarto Trabajo/index.py
@@ -1,3 +1,15 @@
+class Cambio_Octal:
+    @staticmethod
+    def decimal_a_octal(numero):
+     if numero == 0:
+        return "0"
+     octal = ""
+     while numero > 0:
+        residuo = numero % 8
+        octal = str(residuo) + octal
+        numero //= 8
+     return octal
+    
 opcion = 0
 
 while opcion != 4:
@@ -15,7 +27,8 @@ while opcion != 4:
             print(f"El número {numero} en binario es: {bin(numero)[2:]}")
         elif opcion == 2:
             numero = int(input("Ingresa el número: "))
-            print(f"El número {numero} en octal es: {oct(numero)[2:]}")
+            
+            print("el numero: ",numero,"en octal es: ", Cambio_Octal.decimal_a_octal(numero))
         elif opcion == 3:
             numero = int(input("Ingresa el número: "))
             print(f"El número {numero} en hexadecimal es: {hex(numero)[2:]}")
